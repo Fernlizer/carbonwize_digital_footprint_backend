c8f4a8900653cb0592ac66d84daa6dc0d3510f0d
FERN <Fernlizer@hotmail.com>
2025-01-30 04:33:46 +0700

    add route for weight

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,5 +9,10 @@ import (
 // SetupRoutes ตั้งค่า API Routes ทั้งหมด
 func SetupRoutes(app *fiber.App, carbonHandler *handler.CarbonHandler) {
 	api := app.Group("/api/carbon/footprint")
+
+	// คำนวณ Carbon Footprint แบบพื้นฐาน
 	api.Post("/calculate", carbonHandler.CalculateEmission)
+
+	// คำนวณ Carbon Footprint แบบใช้ `weight`
+	api.Post("/calculate/weight", carbonHandler.CalculateEmissionWithWeight)
 }
